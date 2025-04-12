068c6625ced7fd121cfacf1055999fef833190b5
karman07 <84911866+karman07@users.noreply.github.com>
2025-04-12 14:40:39 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.scss";
 import Main from "./containers/Main";
+import AdsTxt from "./pages/AdsTxt";
 
 function App() {
   return (
-    <div>
-      <Main />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/app-ads.txt" element={<AdsTxt />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/AdsTxt.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AdsTxt = () => {
+  const adsTxtContent = `
+google.com, pub-7069180478740738, DIRECT, f08c47fec0942fa0
+`;
+
+  return (
+    <div style={{ whiteSpace: "pre-wrap", fontFamily: "monospace", padding: "1rem" }}>
+      {adsTxtContent}
+    </div>
+  );
+};
+
+export default AdsTxt;
